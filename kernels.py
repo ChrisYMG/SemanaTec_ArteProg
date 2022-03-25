1717e6d6355a4b08790d647d07b7b6311c444485
ChrisYMG <cmejiagalindo@yahoo.com>
2022-03-24 21:07:32 -0600

    Kernels

---
+++ kernels.py
@@ -0,0 +1,55 @@
+import numpy
+
+# MexicanHat Ferza
+def MexicanHat(sigma, K):
+    A=numpy.zeros((K,K)) #Devulve matriz formada por ceros
+    for x in range(0,K):
+        for y in range(0,K):
+            #Formula de MexicanHat
+            A[x][y] = 1/(numpy.pi*sigma**4) * (1-(1/2)*((x**2+y**2)/(sigma**2))) * numpy.exp(-(x**2+y**2)/(2*sigma**2))
+    return A
+
+# TopSobel #Omar
+def TopSobel(K):
+    A = numpy.zeros((K,K))
+    A[K//2][K//2] = 0
+    A[K//2 + 1][K//2] = -2
+    A[K//2 - 1][K//2] = 2
+    A[K//2][K//2 + 1] = 0
+    A[K//2][K//2 - 1] = 0
+    A[K//2 + 1][K//2 + 1] = -1
+    A[K//2 + 1][K//2 - 1] = -1
+    A[K//2 - 1][K//2 + 1] = 1
+    A[K//2 - 1][K//2 - 1] = 1
+    return A
+
+# GaussBLur #Christian
+def GaussBlur(sigma, K):
+    A=numpy.zeros((K,K))
+    for x in range(0,K):
+        for y in range(0,K):
+            A[x][y] = 1/(2*numpy.pi*sigma**2)*numpy.exp(-(x**2+y**2)/(2*sigma**2))
+
+    return A
+
+
+# LaplacianGauss #Jorge
+def LaplacianGauss(sigma, K):
+    A=numpy.zeros((K,K))
+    for x in range(0,K):
+        for y in range(0,K):
+            A[x][y] = -(1/(numpy.pi*sigma**4)) * (1-((x**2+y**2)/(2*sigma**2)))* numpy.exp(-(x**2+y**2)/(2*sigma**2))
+    return A
+
+# Laplace #Ricardo
+def laplace(K):
+    A=numpy.zeros((K,K))
+    A[K//2][K//2] = 4
+    A[K//2 + 1][K//2] = -1
+    A[K//2 - 1][K//2] = -1
+    A[K//2][K//2 + 1] = -1
+    A[K//2][K//2 - 1] = -1
+    return A
+
+
+
---
+++ main.py
@@ -0,0 +1,67 @@
+# Inclusion de las librerias necesarias para el funcionmamiento del codigo.
+
+import matplotlib.pyplot as plt
+import numpy
+from PIL import Image
+from scipy import ndimage
+from kernels import *
+
+# Importacion de las imagenes a ser analizadas.
+
+imgInput = Image.open('mr_increible.jpg')
+I  = imgInput.convert('L')
+I  = numpy.asarray(I)
+I  = I / 255.0
+
+# Padding
+
+def pad_with(vector, pad_width, iaxis, kwargs):
+    pad_value = kwargs.get('padder', 20)
+    vector[:pad_width[0]] = pad_value
+    vector[-pad_width[1]:] = pad_value
+    
+# Procesado de imagines Atravez de los distintos kernels
+
+I = numpy.pad(I, 10, pad_with, padder=0)
+
+kernelMH = MexicanHat(3,9) 
+kernelTS = TopSobel(3) 
+kernelGB = GaussBlur(9,9) 
+kernelLG = LaplacianGauss(9,9)
+kernelLA = laplace(9) 
+
+
+imgTS = ndimage.convolve(I, kernelTS, mode='constant', cval=0.0)
+imgGB = ndimage.convolve(I, kernelGB, mode='constant', cval=0.0) 
+imgMH = ndimage.convolve(I, kernelMH, mode='constant', cval=0.0)
+imgLA = ndimage.convolve(I, kernelLA, mode='constant', cval=0.0)
+imgLG = ndimage.convolve(I, kernelLG, mode='constant', cval=0.0)
+
+# Mostrar los resultados despues de el procesamiento.
+
+
+plt.imshow(imgInput)
+plt.xlabel('Input Image')
+plt.show()
+
+
+plt.imshow(imgTS)
+plt.xlabel('Image Top Sobel')
+plt.show()
+
+plt.imshow(imgGB)
+plt.xlabel('Image Gaussian Blur')
+plt.show()
+
+plt.imshow(imgMH)
+plt.xlabel('Image Ricker Wavelet - Mexican Hat')
+plt.show()
+
+plt.imshow(imgLA)
+plt.xlabel('Image Laplacian')
+plt.show()
+
+plt.imshow(imgLG)
+plt.xlabel('Image Laplacian Gauss')
+plt.show()
+
