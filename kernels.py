5e4e900cc616ac034bba49aacd6955af33047283
A01769948 <102306540+A01769948@users.noreply.github.com>
2022-03-24 21:59:15 -0600

    Update kernels.py

--- kernels.py
+++ kernels.py
@@ -10,7 +10,7 @@ def MexicanHat(sigma, K):
 
 # TopSobel Omar Alejandro Rodríguez Valencia
 def TopSobel(K):
-    A = A = numpy.array([[-2,-1,0],[-1,1,1],[0,1,2]])
+    A = numpy.array([[-2,-1,0],[-1,1,1],[0,1,2]])
     return A
 
 # GaussBLur Christian Yael Mejía Galindo
